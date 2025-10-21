30407d5f97bcf06136b6263e7beda3ae865d395b
Jallen Hunnid <aubinderulo@gmail.com>
2025-10-21 12:19:55 +0200

    Grid layout set

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ let body = document.querySelector('body')
 const gridContainer = document.createElement('div')
 gridContainer.id ="container";
 let container =  document.querySelector('#container')
+let gridSize = 16;
 
-let gridItem = document.createElement('div')
-gridItem.classList.add('gridItem')
-gridItem.textContent = "Item";
+for(let i = 1; i < gridSize * gridSize; i++){
+    let gridItem = document.createElement('div');
+    gridItem.classList.add('gridItem');
+    gridContainer.appendChild(gridItem);
 
-gridContainer.appendChild(gridItem);
+}
+
+
+
+
 body.appendChild(gridContainer)
